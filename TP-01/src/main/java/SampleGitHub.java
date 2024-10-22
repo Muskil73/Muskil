9b6b7d73b4ab31dd53480a24e688843dedcf82d4
Muskil <Muskil@LAPTOP-C4KL1321.mshome.net>
2024-10-22 13:06:28 +0530

    Vanakkam

---
+++ TP-01/src/main/java/SampleGitHub.java
@@ -0,0 +1,12 @@
+
+public class SampleGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("six ku apron seven");
+		System.out.println("Muskil Moracha Emman");
+
+	}
+
+}
